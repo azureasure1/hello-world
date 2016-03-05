70c952fb8c0078036a31b8e98f9c238fbfe33d48
azureasure1 <griffincody38@yahoo.com>
2016-03-04 20:55:40 -0500

    HelloWorld.cpp
    This is the hello world program in C++.

---
+++ HelloWorld.cpp
@@ -0,0 +1,19 @@
+/*
+Programmer: Cody Griffin
+Date: 3/4/2016
+Program: Hello World
+Purpose: To print "Hello World!" to the console.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//The main function
+int main(){
+	cout << "Hello World!" << endl;
+	
+	//Pauses the console
+	cin.get();
+	return 0;
+}//end main
