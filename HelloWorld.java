86164d03ff3122d2043608655288d7489bc6c6ac
Cody Griffin <griffincody38@yahoo.com>
2016-07-06 15:02:44 -0400

    HelloWorld.java
    This is the "Hello World" program done in Java.

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Programmer: Cody Griffin
+ * Date: 7/6/2016
+ * Program: Hello World
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}//End main
+
+}//End class
